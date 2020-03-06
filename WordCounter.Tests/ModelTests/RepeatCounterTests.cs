fcea20f301658d375a842d25b65ac616fed604d7
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-06 10:30:03 -0800

    add test to set first string value

--- WordCounter.Tests/ModelTests/RepeatCounterTests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -5,6 +5,13 @@ namespace WordCounter.TestTools{
   [TestClass]
   public class RepeatCounterTests
   {
-    
+    [TestMethod]
+    public void WordCounter_SetGetString_String()
+    {
+      RepeatCounter testCounter = new RepeatCounter();
+      string testWord = "test";
+      testCounter.SetInputWord(testWord);
+      Assert.AreEqual(testCounter.InputWord, "test");
+    }
   }
 }
